a937d70f93341966d4ef292e1f97781417febb7c
ArhamAkhtarAlam <132830963+ArhamAkhtarAlam@users.noreply.github.com>
2024-12-31 01:05:27 +0530

    reverse application and other stuff

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
-distance = 0
-start_time = 0
-stop_time = 0
-running_time = 0
-speed_ = 100
-diameter = 1
+def on_logo_long_pressed():
+    control.reset()
+input.on_logo_event(TouchButtonEvent.LONG_PRESSED, on_logo_long_pressed)
 
 def on_button_pressed_a():
     global start_time
     start_time = input.running_time() / 1000
     servos.P0.run(speed_)
 input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_ab():
+    global switch_for_reverse
+    if switch_for_reverse == 1:
+        switch_for_reverse = 0
+    else:
+        switch_for_reverse = 1
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
 def on_button_pressed_b():
     global stop_time, running_time
@@ -20,36 +25,27 @@ input.on_button_pressed(Button.B, on_button_pressed_b)
 
 def on_logo_pressed():
     basic.show_number(distance)
+    basic.pause(200)
+    basic.clear_screen()
 input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_pressed)
+
+distance = 0
+running_time = 0
+stop_time = 0
+switch_for_reverse = 0
+speed_ = 0
+start_time = 0
+diameter = 1
 
 def on_forever():
     global distance
     distance = Math.round(running_time * (2 * (3.1415926585 * (diameter / 2))) * (speed_ / 100))
 basic.forever(on_forever)
 
-# input.onButtonPressed(Button.A, function () {
-# servos.P0.run(speed_)
-# })
-# input.onButtonPressed(Button.B, function () {
-# stoped_time = times
-# running_time = times - (times - stoped_time) + past_running_time
-# past_running_time = running_time
-# servos.P0.run(0)
-# })
-# input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-# basic.showNumber(running_time)
-# })
-# let distance = 0
-# let past_running_time = 0
-# let times = 0
-# let stoped_time = 0
-# let running_time = 0
-# let speed_ = 0
-# speed_ = 100
-# let diameter = 1
-# running_time = 0
-# stoped_time = 0
-# basic.forever(function () {
-# times = input.runningTime() / 1000
-# distance = Math.round(running_time * (2 * (3.1415926585 * (diameter / 2))) * (speed_ / 100))
-# })
+def on_forever2():
+    global speed_
+    if switch_for_reverse == 0:
+        speed_ = 100
+    else:
+        speed_ = -100
+basic.forever(on_forever2)
--- main.ts
+++ main.ts
@@ -1,24 +1,41 @@
-let distance = 0
-let start_time = 0
-let stop_time = 0
-let running_time = 0
-let speed_ = 100
-let diameter = 1
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    
+input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
+    control.reset()
+})
+input.onButtonPressed(Button.A, function () {
     start_time = input.runningTime() / 1000
     servos.P0.run(speed_)
 })
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
-    
+input.onButtonPressed(Button.AB, function () {
+    if (switch_for_reverse == 1) {
+        switch_for_reverse = 0
+    } else {
+        switch_for_reverse = 1
+    }
+})
+input.onButtonPressed(Button.B, function () {
     stop_time = input.runningTime() / 1000
     running_time += stop_time - start_time
     servos.P0.run(0)
 })
-input.onLogoEvent(TouchButtonEvent.Pressed, function on_logo_pressed() {
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     basic.showNumber(distance)
+    basic.pause(200)
+    basic.clearScreen()
 })
-basic.forever(function on_forever() {
-    
+let distance = 0
+let running_time = 0
+let stop_time = 0
+let switch_for_reverse = 0
+let speed_ = 0
+let start_time = 0
+let diameter = 1
+basic.forever(function () {
     distance = Math.round(running_time * (2 * (3.1415926585 * (diameter / 2))) * (speed_ / 100))
+})
+basic.forever(function () {
+    if (switch_for_reverse == 0) {
+        speed_ = 100
+    } else {
+        speed_ = -100
+    }
 })
