e1304fa271056ddaa9267c0e6dcae31fb4c88452
ArhamAkhtarAlam <132830963+ArhamAkhtarAlam@users.noreply.github.com>
2024-12-30 22:15:18 +0530

    Update pxt.json, main.blocks, main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,24 +1,24 @@
-input.onButtonPressed(Button.A, function () {
-    running_time = times - (times - stoped_time)
+let distance = 0
+let start_time = 0
+let stop_time = 0
+let running_time = 0
+let speed_ = 100
+let diameter = 1
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
+    start_time = input.runningTime() / 1000
     servos.P0.run(speed_)
 })
-input.onButtonPressed(Button.B, function () {
-    stoped_time = times
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
+    stop_time = input.runningTime() / 1000
+    running_time += stop_time - start_time
     servos.P0.run(0)
 })
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+input.onLogoEvent(TouchButtonEvent.Pressed, function on_logo_pressed() {
     basic.showNumber(distance)
 })
-let distance = 0
-let times = 0
-let stoped_time = 0
-let running_time = 0
-let speed_ = 0
-speed_ = 100
-let diameter = 1
-running_time = 0
-stoped_time = 0
-basic.forever(function () {
-    times = input.runningTime() / 100
+basic.forever(function on_forever() {
+    
     distance = Math.round(running_time * (2 * (3.1415926585 * (diameter / 2))) * (speed_ / 100))
 })
